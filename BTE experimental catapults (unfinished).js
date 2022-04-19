baeaff50ee8d75982f90d796f3c5b4faf7d4a851
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-04-19 15:33:28 -0700

    Update BTE experimental catapults (unfinished).js

--- BTE experimental catapults (unfinished).js
+++ BTE experimental catapults (unfinished).js
@@ -1,8 +1,12 @@
 var LLA = geofs.aircraft.instance.llaLocation;
+var hT = geofs.aircraft.instance.parts.engine.thrust / 100
+var hABT = geofs.aircraft.instance.parts.engine.afterBurnerThrust / 100
+var T = geofs.aircraft.instance.parts.engine.thrust
+var ABT = geofs.aircraft.instance.parts.engine.afterBurnerThrust
 
 /*
 I don't know if GeoFS has a stock way to have a range of LLAs,
-but we could probably encode it into a single number that can be within a value range.
+and I don't really have a solid idea on how to calculate whether an aircraft is in one.
 */
 
 //if (geofs.aircraft.instance.llaLocation == whatever goes here){   //LLA "if" statement for where carrier catapults are
@@ -10,27 +14,26 @@ but we could probably encode it into a single number that can be within a value 
 //aircraft won't move if catapult is engaged and brakes are on, but only for 1 minute
 document.addEventListener("keydown", function(e) {
 if (e.keycode == /*shift+q*/ ) {
+T = (T / 100)
+ABT = (ABT / 100)
 setTimeout(() => {
-geofs.aircraft.instance.parts.engine.thrust = (geofs.aircraft.instance.parts.engine.thrust / 100)
-geofs.aircraft.instance.parts.engine.thrust = (geofs.aircraft.instance.parts.engine.afterBurnerThrust / 100)
+T = (hT * 100)
+ABT = (hABT * 100)
    }, 60000)
 };
 
-var hT = geofs.aircraft.instance.parts.engine.thrust / 100
-var hABT = geofs.aircraft.instance.parts.engine.afterBurnerThrust / 100
-
-if (geofs.aircraft.instance.id == 7 || geofs.aircraft.instance.id == 2581){
-if (geofs.aircraft.instance.parts.engine.thrust == hT || geofs.aircraft.instance.parts.engine.afterBurnerThrust == hABT){
+if (geofs.aircraft.instance.id == 7){   // < || geofs.aircraft.instance.id == 2581 >    //F-14
+if (T == hT || ABT == hABT){
    document.addEventListener("keydown", function(e) {
 	   if (e.keyCode == 81) {
 	   if (geofs.animation.values.groundContact == 1){
-		   if (geofs.aircraft.instance.parts.engine.thrust == hT){
-			geofs.aircraft.instance.parts.engine.thrust = (hT * 100)
+		   if (T == hT){
+			T = (hT * 100)
 			}
-		   if (geofs.aircraft.instance.parts.engine.afterBurnerThrust == hABT){
-			geofs.aircraft.instance.parts.engine.thrust = (hABT * 100)
+		   if (ABT == hABT){
+			ABT = (hABT * 100)
 			}
-	   geofs.aircraft.instance.parts.engine.afterBurnerThrust = (geofs.aircraft.instance.parts.engine.afterBurnerThrust * 4)
+	   ABT = (ABT * 4)
 		let whiteSmokeEmitter = new geofs.fx.ParticleEmitter({
             anchor: {
                         worldPosition: [0, 0, -1]
@@ -48,7 +51,7 @@ if (geofs.aircraft.instance.parts.engine.thrust == hT || geofs.aircraft.instance
             startRotation: "random",
             texture: "whitesmoke"
         })
-	   setTimeout(() => {geofs.aircraft.instance.parts.engine.afterBurnerThrust = (geofs.aircraft.instance.parts.engine.afterBurnerThrust / 4)}, 2000);
+	   setTimeout(() => {ABT = (ABT / 4)}, 2000);
 		setTimeout(() => {whiteSmokeEmitter.destroy()}, 2250);
 	   }
 	   }
